0d3f9774174a0630171e89b4c5a8d39f3f1927e6
akm77 <aleksey.kotryakhov@gmail.com>
2023-03-01 19:39:34 +1000

    Refactor dictionary CRUD

--- tgbot/handlers/user.py
+++ tgbot/handlers/user.py
@@ -2,13 +2,14 @@ from aiogram import Dispatcher
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message
 
+from tgbot.dialogs.main_menu.states import MainMenu
 from tgbot.dialogs.shift_menu.states import ShiftMenu
 from tgbot.widgets.aiogram_dialog import DialogManager
 
 
 async def user_start(message: Message, dialog_manager: DialogManager, state: FSMContext, **kwargs):
-    await dialog_manager.start(ShiftMenu.select_shift)
+    await dialog_manager.start(MainMenu.select_action)
 
 
 def register_user(dp: Dispatcher):
-    dp.register_message_handler(user_start, commands=["shift"], state="*")
+    dp.register_message_handler(user_start, commands=["start"], state="*")
